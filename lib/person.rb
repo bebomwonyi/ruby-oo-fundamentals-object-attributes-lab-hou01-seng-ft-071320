8cc5af78a720c06fda332935bca14f52b8cf7ecc
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-14 18:57:17 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,20 @@
 class Person
- 
   def initialize(name)
     @name = name
   end
- 
-  def name=(full_name)
+  def name=(name)
     @name = name
   end
- 
   def name
     "#{@name}"
   end
- 
+  def initialize(job)
+    @job = job
+  end
+  def job=(j)
+    @job = j
+  end
+  def job
+    "#{job}"
+  end
 end
